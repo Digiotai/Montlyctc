9007441c6abab3a62f7a19abbcb890cd8ae91b0a
Digiotai <55736185+Digiotai@users.noreply.github.com>
2019-11-14 14:19:16 +0530

    Update app.py

--- Montly CTC Prediction/CTC/MONTLYCTC/app.py
+++ Montly CTC Prediction/CTC/MONTLYCTC/app.py
@@ -1,31 +1,14 @@
-#step -1 # Importing flask module in the project is mandatory 
-
 import numpy as np
 from flask import Flask, request, jsonify, render_template
 import pickle
-from sklearn.svm import SVR
-
-
-
-
-#Step -2 Flask constructor takes the name of  
-# current module (__name__) as argument.app = Flask(__name__)
 
 app = Flask(__name__)
-
-#Step -3 Load Trained Regression Model
 model = pickle.load(open('SVR.pkl', 'rb'))
-
-
-# Step -5 The route() function of the Flask class is a decorator,  
-# which tells the application which URL should call  
-# the associated function
-
 
 @app.route('/')
 def home():
     return render_template('index.html')
-# ‘/’ URL is bound with predict() function.
+
 @app.route('/predict',methods=['POST'])
 def predict():
     '''
@@ -33,27 +16,17 @@ def predict():
     '''
     User =request.form['msg']
     
-   
     user = np.array(User)
     user = user.reshape(-1,1)
     user = model.predict(user)
-    print("Candidate Position Level:{}, Montly CTC {}INR " .format(User,user))
     
-                                
-    
-    
-   
     
 
     
 
-    return render_template('index.html', prediction_text="Candidate Position Level:{}, Montly CTC {}INR " .format(User,user)
+    return render_template('index.html', prediction_text="Candidate Position {},  Montly CTC {}INR " .format(User,user))
+    #return jsonify(res)
 
 
-# main driver function
- # run() method of Flask class runs the application  
-    # on the local development server.
-    
-
-    if __name__ == '__main__':
-        app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
